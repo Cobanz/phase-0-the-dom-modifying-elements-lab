e9e3fb55b5b4c71685055dda938643ff337419ff
Charles Grant Nichols <grant@inkdots.com>
2021-03-17 18:17:47 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const removeMain = document.querySelector('main').remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory'
+
+newHeader.textContent = " Grant is the champion"
